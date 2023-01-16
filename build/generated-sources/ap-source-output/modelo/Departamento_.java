4fdad8baffefe5f4055216e0ef88354aa7a21c5d
WI1L0 <111997657+WI1L0@users.noreply.github.com>
2023-01-15 21:19:02 -0500

    Add files via upload

---
+++ build/generated-sources/ap-source-output/modelo/Departamento_.java
@@ -0,0 +1,17 @@
+package modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import modelo.Empleado;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-01-15T16:06:20")
+@StaticMetamodel(Departamento.class)
+public class Departamento_ { 
+
+    public static volatile SingularAttribute<Departamento, Integer> idDep;
+    public static volatile CollectionAttribute<Departamento, Empleado> empleadoCollection;
+    public static volatile SingularAttribute<Departamento, String> nombre;
+
+}
---
+++ build/generated-sources/ap-source-output/modelo/Empleado_.java
@@ -0,0 +1,16 @@
+package modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import modelo.Departamento;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-01-15T16:06:20")
+@StaticMetamodel(Empleado.class)
+public class Empleado_ { 
+
+    public static volatile SingularAttribute<Empleado, Departamento> idDepEmp;
+    public static volatile SingularAttribute<Empleado, Integer> idEmp;
+    public static volatile SingularAttribute<Empleado, String> nombre;
+
+}
